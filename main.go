876a05301983146b326091d31f5e69c640a8f63e
WinterPhish <trimasitisu.battle@gmail.com>
2025-11-05 23:29:10 -0500

    Player can shoot

--- main.go
+++ main.go
@@ -16,22 +16,31 @@ const (
 
 var (
 	playerImage *ebiten.Image
+	bulletImage *ebiten.Image
 )
 
 type Game struct {
 	player   Player
+	bullet   *Bullet
 	ennemies []string
 }
 
 type Player struct {
-	playerX int
-	vX      int
-	vY      int
+	playerX  int
+	vPlayerX int
+	vPlayerY int
+}
+
+type Bullet struct {
+	bulletX  int
+	bulletY  int
+	vBulletY int
 }
 
 func (g *Game) Update() error {
 	g.keyPressed()
 	g.player.update()
+	g.bullet.update(g)
 	return nil
 }
 
@@ -42,22 +51,29 @@ func (g *Game) keyPressed() {
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		moveLeft(g)
 	}
+	if ebiten.IsKeyPressed(ebiten.KeyZ) {
+		shoot(g)
+	}
 }
 
 func moveRight(g *Game) {
-	g.player.vX += 8
-	print("move right")
+	g.player.vPlayerX += 4
 }
 
 func moveLeft(g *Game) {
-	g.player.vX -= 8
-	print("move left")
+	g.player.vPlayerX -= 4
+}
+
+func shoot(g *Game) {
+	if g.bullet == nil {
+		g.bullet = &Bullet{bulletX: g.player.playerX, bulletY: playerY, vBulletY: -4}
+	}
 }
 
 func (p *Player) update() {
-	p.playerX += p.vX
-	if p.vX != 0 {
-		p.vX = 0
+	p.playerX += p.vPlayerX
+	if p.vPlayerX != 0 {
+		p.vPlayerX = 0
 	}
 }
 
@@ -67,18 +83,41 @@ func (p *Player) draw(screen *ebiten.Image) {
 	screen.DrawImage(playerImage, op)
 }
 
+func (b *Bullet) update(g *Game) {
+	if b == nil {
+		return
+	}
+	b.bulletY += b.vBulletY
+	if b.bulletY < 0 {
+		g.bullet = nil
+	}
+}
+
+func (b *Bullet) draw(screen *ebiten.Image) {
+	if b == nil {
+		return
+	}
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(b.bulletX), float64(b.bulletY))
+	screen.DrawImage(bulletImage, op)
+}
+
 func init() {
 	img, _, err := ebitenutil.NewImageFromFile("assets/player.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	playerImage = ebiten.NewImageFromImage(img)
+	img, _, err = ebitenutil.NewImageFromFile("assets/bullet.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bulletImage = ebiten.NewImageFromImage(img)
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(frameWidth/2-16, frameHeight/2+frameHeight/4)
 	g.player.draw(screen)
+	g.bullet.draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
